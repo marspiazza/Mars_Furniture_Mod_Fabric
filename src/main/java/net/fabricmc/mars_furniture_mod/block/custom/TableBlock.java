13a3586e2682143b7cd21408882f8f60beaa7c64
Mars Piazza <epiazza@mines.edu>
2022-10-13 12:37:28 -0600

    fixed texture and bounding box issues on table git add -A

--- src/main/java/net/fabricmc/mars_furniture_mod/block/custom/TableBlock.java
+++ src/main/java/net/fabricmc/mars_furniture_mod/block/custom/TableBlock.java
@@ -16,6 +16,6 @@ public class TableBlock extends Block {
     
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-        return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 1.0f, 0.5f);
+        return VoxelShapes.cuboid(0f, 0.1f, 0f, 1.0f, 1.0f, 1.0f);
     }
 }
