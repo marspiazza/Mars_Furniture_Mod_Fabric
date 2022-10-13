ae017aa95ec92bb884fc6820063feb5f4c171439
Mars Piazza <epiazza@mines.edu>
2022-10-12 21:54:13 -0600

    added in acacia_table, currently the texture and collision are a lil funky but the game doesnt immediately crash so im calling that a win

--- src/main/java/net/fabricmc/mars_furniture_mod/Mars_Furniture_Mod.java
+++ src/main/java/net/fabricmc/mars_furniture_mod/Mars_Furniture_Mod.java
@@ -1,6 +1,15 @@
 package net.fabricmc.mars_furniture_mod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.mars_furniture_mod.block.custom.TableBlock;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,11 +19,18 @@ public class Mars_Furniture_Mod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("mars_furniture_mod");
 
+	//BLOCKS
+	//ACACIA
+	public static final TableBlock ACACIA_TABLE = new TableBlock(FabricBlockSettings.of(Material.WOOD));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+
+		//ACACIA
+		Registry.register(Registry.BLOCK, new Identifier("mars_furniture_mod", "acacia_table"), ACACIA_TABLE);
+        Registry.register(Registry.ITEM, new Identifier("mars_furniture_mod", "acacia_table"), new BlockItem(ACACIA_TABLE, new Item.Settings().group(ItemGroup.MISC)));
+
 
 		LOGGER.info("Hello Fabric world!");
 	}
---
+++ src/main/java/net/fabricmc/mars_furniture_mod/block/custom/TableBlock.java
@@ -0,0 +1,21 @@
+package net.fabricmc.mars_furniture_mod.block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+
+public class TableBlock extends Block {
+
+    public TableBlock(Settings settings){
+        super(settings);
+    }
+    
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        return VoxelShapes.cuboid(0f, 0f, 0f, 1f, 1.0f, 0.5f);
+    }
+}
